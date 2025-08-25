b30ef89818ea6effaf01ff8ba5233f976c973a70
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2025-08-25 19:40:22 -0300

    Implementação - React
    Implementação da tecnologia -  React ao projeto "Register"

--- User/register/src/App.jsx
+++ User/register/src/App.jsx
@@ -10,7 +10,32 @@ function App() {
     <>
       <div className="register">
         <div className="container">
-          <div className="info">
+
+          <div className='login'>
+            <span className='login-tittle'>Faça login em sua conta Empire.</span>
+            <span className='login-msg'>Continue se Desenvolvendo profissionalmente.</span>
+            <form className="login-form">
+              <div className="login-form-div">
+                <label className="login-form-div-label" htmlFor="email" ><i className="material-symbols-outlined">mail</i></label>
+                <input className="login-form-div-input" type="email"  name='email' id='email' placeholder="Email"/>
+              </div>
+              <div className="login-form-div">
+                <label className="login-form-div-label" htmlFor="password"><i className="material-symbols-outlined">lock</i></label>
+                <input className="login-form-div-input" type="password"  name='password' id='password' placeholder="Senha"/>
+              </div>
+              <div className="login-form-button">
+                <button className="login-form-div-button">Fazer login</button>
+              </div>
+            </form>
+          </div>
+          <div className="info-login">
+            <img className="info-login-logo" src={logo} alt=""  />
+            <span className="info-login-tittle">Não tem uma conta?</span>
+            <span className="info-login-msg">Crie uma conta e explore nossos recursos!</span>
+            <button className="info-login-button">Criar conta</button>
+          </div>
+
+          {/* <div className="info">
             <img className="info-logo" src={logo} alt=""  />
             <span className="info-tittle">Já tem uma conta?</span>
             <span className="info-msg">Faça login e continue de onde parou!</span>
@@ -36,7 +61,8 @@ function App() {
                 <button className="create-form-div-button">Criar conta</button>
               </div>
             </form>
-          </div>
+          </div> */}
+
         </div>
       </div>
     </>
